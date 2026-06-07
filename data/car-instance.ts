3a44060ac236f41bcd6cec103c93704846b3142b
Mykola Nykolaichuk <52537767+mnykolaichuk96@users.noreply.github.com>
2026-06-07 19:19:52 +0200

    Car instances status update

--- data/car-instance.ts
+++ data/car-instance.ts
@@ -34,7 +34,7 @@ export const carInstances: CarInstance[] = [
         brand: "Toyota",
         model: "Auris",
         generation: "I",
-        status: "available",
+        status: "rented",
         equipment: [
             "rear_camera",
             "pdc",
@@ -56,7 +56,7 @@ export const carInstances: CarInstance[] = [
         brand: "Honda",
         model: "Insight",
         generation: "I",
-        status: "available",
+        status: "rented",
         equipment: [
             "rear_camera",
             "pdc",
